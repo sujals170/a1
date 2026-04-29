0ede74f9aa421c377b9366fd3fad4bf6f014a668
Sujal Dhaduk <167689197+sujals170@users.noreply.github.com>
2026-04-29 21:06:08 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,21 +1,30 @@
 #!/usr/bin/env bash
 set -e
 
+# Strip any accidental newlines or whitespace from env vars
+_apiKey=$(echo "${apiKey:-}" | tr -d '\r\n\t ')
+_authDomain=$(echo "${authDomain:-}" | tr -d '\r\n\t ')
+_databaseURL=$(echo "${databaseURL:-}" | tr -d '\r\n\t ')
+_projectId=$(echo "${projectId:-}" | tr -d '\r\n\t ')
+_storageBucket=$(echo "${storageBucket:-}" | tr -d '\r\n\t ')
+_messagingSenderId=$(echo "${messagingSenderId:-}" | tr -d '\r\n\t ')
+_appId=$(echo "${appId:-}" | tr -d '\r\n\t ')
+
 node -e "
 const fs = require('fs');
 const config = \`window.APP_CONFIG = {
   firebase: {
-    apiKey: '${apiKey:-}',
-    authDomain: '${authDomain:-}',
-    databaseURL: '${databaseURL:-}',
-    projectId: '${projectId:-}',
-    storageBucket: '${storageBucket:-}',
-    messagingSenderId: '${messagingSenderId:-}',
-    appId: '${appId:-}'
+    apiKey: '${_apiKey}',
+    authDomain: '${_authDomain}',
+    databaseURL: '${_databaseURL}',
+    projectId: '${_projectId}',
+    storageBucket: '${_storageBucket}',
+    messagingSenderId: '${_messagingSenderId}',
+    appId: '${_appId}'
   }
 };\`;
 let html = fs.readFileSync('index.html', 'utf8');
 html = html.replace('<!-- FIREBASE_CONFIG -->', '<script>' + config + '</script>');
 fs.writeFileSync('index.html', html);
-console.log('Firebase config injected into index.html');
+console.log('Firebase config injected successfully');
 "
